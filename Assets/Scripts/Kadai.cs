635e2ebe449189427b084352438e6229e30d095a
shunJS <75880904+shunJS@users.noreply.github.com>
2021-10-10 23:43:39 +0900

    modify Kadai script

--- Assets/Scripts/Kadai.cs
+++ Assets/Scripts/Kadai.cs
@@ -2,12 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Kadai : MonoBehaviour
 {
-   
-    void OnBecameInvisible()
+    private GameObject unitychan;
+    public GameObject cam;
+    public GameObject fab;
+    private float difference;
+
+    void Start()
     {
-        // 表示されなくなった時の処理
-        GameObject.Destroy(this.gameObject);
+
+        this.cam = GameObject.Find("Main Camera");
+        this.fab = this.gameObject;
+
+        //this.difference = cam.transform.position.z - fab.transform.position.z;
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fab.transform.position.z < cam.transform.position.z)
+        {
+            GameObject.Destroy(fab);
+        }
+
+
+
+    }
+
+   
 }
